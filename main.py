95c13797d8ebefa4877bf12d65c5bf86e3a5ad28
furpman <71867715+furpman@users.noreply.github.com>
2021-04-28 23:20:46 -0600

    -created a Circle module that will take care of everything circle -changed up the main test program to be a car

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Circle import *
 import math
 import os.path
 import random
@@ -5,19 +6,17 @@ import pygame
 import numpy
 
 # load an image of the smiley dude
-SPR_SMILEGUY = pygame.image.load(os.path.join("sprites", "smileguy.png"))
+SPR_SOME_CAR = pygame.image.load(os.path.join("sprites", "car.png"))
 # stretch out the sprite
-SPR_SMILEGUY = pygame.transform.scale(SPR_SMILEGUY, (64,64))
-# the speed of smile dude
-SMILEGUY_SPEED = 2
+SPR_SOME_CAR = pygame.transform.scale(SPR_SOME_CAR, (32,64))
 
 # width and height of the game window
 WIDTH, HEIGHT = 640, 640
 # create the game window
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 # set the title and icon of the game window
-pygame.display.set_caption("Lil' Smile Dude")
-pygame.display.set_icon(SPR_SMILEGUY)
+pygame.display.set_caption("Gamegame")
+pygame.display.set_icon(SPR_SOME_CAR)
 
 # how many frames per second the game will run at
 FPS = 60
@@ -25,30 +24,6 @@ FPS = 60
 # define some colours by their rgb values
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-
-class Circle:
-    def __init__(self, x, y, sprite):
-        # the sprite of this circle
-        self.sprite = sprite
-        # the colour of this circle
-        self.colour = BLACK
-        # the x and y positions of the center of this circle
-        self.x = x
-        self.y = y
-        # radius of this circle
-        self.r = 0
-        # speed that this circle is travelling
-        self.spd = 0
-        # direction in radians that this circle is travelling (0 = east)
-        self.dir = 0
-        # the horizontal and vertical speeds of this circle
-        self.hspd = 0
-        self.vspd = 0
-        # the friction experienced by this circle
-        self.friction = 0
-        # the gravity experienced by this circle
-        self.grav = 0
 
 
 def draw_everything(instances):
@@ -69,9 +44,9 @@ def main():
     instances = []
 
     # create an instance for a smiley dude and add it to instances
-    smileguy = Circle(WIDTH/2, HEIGHT/2, SPR_SMILEGUY)
-    smileguy.r = 32
-    instances.append(smileguy)
+    some_car = Circle(WIDTH/2, HEIGHT/2, SPR_SOME_CAR)
+    some_car.r = 32
+    instances.append(some_car)
 
     # create a pygame clock (used to limit the game to run at a certain fps)
     clock = pygame.time.Clock()
@@ -90,59 +65,12 @@ def main():
         # get an array of the currently pressed keys
         keys_pressed = pygame.key.get_pressed()
 
-        # space spawns a guy in the center of the screen
-        if keys_pressed[pygame.K_SPACE]:
-            xoffset = random.randint(-10, 10)
-            yoffset = random.randint(-10, 10)
-            inst = Circle(WIDTH / 2 + xoffset, HEIGHT / 2 + yoffset, SPR_SMILEGUY)
-            inst.r = 32
-            instances.append(inst)
-
-        # move the smile dude depending on what key is pressed
-        if keys_pressed[pygame.K_RIGHT]:
-            smileguy.x += SMILEGUY_SPEED
-        if keys_pressed[pygame.K_UP]:
-            smileguy.y -= SMILEGUY_SPEED
-        if keys_pressed[pygame.K_LEFT]:
-            smileguy.x -= SMILEGUY_SPEED
-        if keys_pressed[pygame.K_DOWN]:
-            smileguy.y += SMILEGUY_SPEED
-
         # get the position of the mouse
         mousepos = pygame.mouse.get_pos()
 
         # snap the dude's position to the mouse
-        smileguy.x = mousepos[0]
-        smileguy.y = mousepos[1]
-
-        # circle collision:
-        for i in range(len(instances)):
-            for j in range(i+1, len(instances)):
-                # get the i-th and j-th circles
-                c1 = instances[i]
-                c2 = instances[j]
-
-                # calculate the x and y distances between c1 and c2
-                dx = c2.x - c1.x
-                dy = c2.y - c1.y
-                # and calculate the distance directly between their two centers
-                dist = math.sqrt(pow(dx,2) + pow(dy,2))
-
-                # if the distance is less than the sum of their two radii,
-                # we have a collision
-                if dist < c1.r + c2.r:
-                    # calculate the angle from the line between c1-c2 to the x-axis
-                    theta = math.asin(abs(dy) / dist)
-                    # calculate the overlap between c1 and c2
-                    overlap = (c1.r + c2.r) - dist
-                    overlap_x = (overlap/2) * math.cos(theta)
-                    overlap_y = (overlap/2) * math.sin(theta)
-
-                    #
-                    c1.x += (overlap_x) * -numpy.sign(dx)
-                    c1.y += (overlap_y) * -numpy.sign(dy)
-                    c2.x += (overlap_x) * numpy.sign(dx)
-                    c2.y += (overlap_y) * numpy.sign(dy)
+        some_car.x = mousepos[0]
+        some_car.y = mousepos[1]
 
         # draw the contents of the window
         draw_everything(instances)
