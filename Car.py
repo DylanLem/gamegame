b96f5a694b737f4a198380c7a758abc818db840f
furpman <71867715+furpman@users.noreply.github.com>
2021-04-29 01:01:55 -0600

    more willy

--- Car.py
+++ Car.py
@@ -7,12 +7,10 @@ class Car(Circle.Circle):
     acceleration = 1.0
     orientation = 0.0
 
-    def __init__(self,x,y,sprite):
-        super(Car, self).__init__(x,y,sprite)
+    def __init__(self, x, y, sprite):
+        super(Car, self).__init__(x, y, sprite)
 
-    def accelerate(self,direction, fps):
-        self.velocity += numpy.array([self.acceleration * math.cos(direction) /fps, self.acceleration * -math.sin(direction) / fps])
-
+    def accelerate(self, direction, fps):
         self.velocity += numpy.array([self.acceleration * math.cos(direction) / fps,
                                       self.acceleration * -math.sin(direction) / fps])
-        self.orientation = ((direction - math.pi/2)/(2*math.pi)) * 360
+        self.orientation = ((direction - math.pi / 2) / (2 * math.pi)) * 360
